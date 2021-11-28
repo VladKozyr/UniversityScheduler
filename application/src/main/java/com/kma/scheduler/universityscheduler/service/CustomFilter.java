60a893b53fd53d29a4cd828ce3720e15a6839b5f
Stanislav Bohuta <stanislav.bohuta@kingfisher.com>
2021-11-28 12:20:48 +0200

    add auth and filter

---
+++ application/src/main/java/com/kma/scheduler/universityscheduler/service/CustomFilter.java
@@ -0,0 +1,22 @@
+package com.kma.scheduler.universityscheduler.service;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CustomFilter implements Filter
+{
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        System.out.println("Request URI is: " + req.getRequestURI());
+        chain.doFilter(request, response);
+        System.out.println("Response Status Code is: " + res.getStatus());
+    }
+}
